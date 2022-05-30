00aa9750fed32b0e92a7a71271f70ccfd133027c
robinNcode <msmrobin518@gmail.com>
2022-05-30 19:20:23 +0600

    Chi test

---
+++ chi_square_test.py
@@ -0,0 +1,28 @@
+#chi square test inputing directly value of Oi
+
+totalClass = int(input("Enter total class: "))
+Oi = []
+N = 0
+Dobs = 0
+
+for ptr in range(totalClass):
+  tempOi = float(input(f"Enter Oi for class no {ptr+1}: "))
+  Oi.append(tempOi)
+  N += tempOi
+
+print("All Oi :", Oi)
+Ei = N / totalClass
+
+for datum in Oi:
+  Dobs += pow((datum - Ei), 2) / Ei
+
+print("D obs is: ", Dobs)
+
+Dcritical = float(input("Enter D critical: "))
+
+if Dobs <= Dcritical:
+  print("Test result is Accepted")
+else:
+  print("Test result is Rejected")
+
+
