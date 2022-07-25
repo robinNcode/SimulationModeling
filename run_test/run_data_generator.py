e785171a35080e29d9a69e914abc622a46bf7322
robinNcode <msmrobin518@gmail.com>
2022-07-26 00:59:53 +0600

    run test algorithm done

--- run_test/run_data_generator.py
+++ run_test/run_data_generator.py
@@ -4,7 +4,7 @@ import random
 def generate(filename):
     fileHandler = open(f"{filename}.txt", "w")
 
-    for datum in range(100):
+    for datum in range(40):
         fileHandler.write(f"{round(random.random(), 3)}\n")
 
 
